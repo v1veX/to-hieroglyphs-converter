0e4cb66ebd439018501d022b0887bfc384011d9c
v1veX <vladimir.melov.2004@gmail.com>
2024-10-01 13:48:21 +1000

    refactor variables names

--- converter.js
+++ converter.js
@@ -59,26 +59,26 @@ function convert() {
         }
     }
 
-    const resultField = document.querySelector('.result-field');
-    resultField.textContent = resultValue;
+    const resultFieldElement = document.querySelector('.result-field');
+    resultFieldElement.textContent = resultValue;
     
     addToHistory(inputValue);
     showCopyButton();
 }
 
 function clear() {
-    const inputField = document.querySelector('.converter-input');
-    const resultField = document.querySelector('.result-field');
-    inputField.value = '';
-    resultField.textContent = '';
+    const inputFieldElement = document.querySelector('.converter-input');
+    const resultFieldElement = document.querySelector('.result-field');
+    inputFieldElement.value = '';
+    resultFieldElement.textContent = '';
 
     hideCopyButton();
     showSymbolsAmount();
 }
 
 function copy() {
-    const resultField = document.querySelector('.result-field');
-    const valueToCopy = resultField.textContent;
+    const resultFieldElement = document.querySelector('.result-field');
+    const valueToCopy = resultFieldElement.textContent;
 
     navigator.clipboard.writeText(valueToCopy)
     .then(function() {
@@ -91,22 +91,22 @@ function copy() {
 }
 
 function showCopyButton() {
-    const copyButton = document.querySelector('.copy-button');
-    copyButton.classList.add('shown');
+    const copyButtonElement = document.querySelector('.copy-button');
+    copyButtonElement.classList.add('shown');
 }
 
 function hideCopyButton() {
-    const copyButton = document.querySelector('.copy-button');
-    copyButton.classList.remove('shown');
+    const copyButtonElement = document.querySelector('.copy-button');
+    copyButtonElement.classList.remove('shown');
 }
 
 function showNotification(message) {
-    const notification = document.querySelector('.notification');
-    const notificationText = document.querySelector('.notification-text');
-    notificationText.textContent = message;
-    notification.classList.add('shown');
+    const notificationElement = document.querySelector('.notification');
+    const notificationTextElement = document.querySelector('.notification-text');
+    notificationTextElement.textContent = message;
+    notificationElement.classList.add('shown');
 
-    setTimeout(() => notification.classList.remove('shown'), 2000);
+    setTimeout(() => notificationElement.classList.remove('shown'), 2000);
 }
 
 function addToHistory(data) {
@@ -124,38 +124,38 @@ function addToHistory(data) {
 
 function loadHistory() {
     const history = JSON.parse(localStorage.getItem('history'));
-    const historyList = document.querySelector('.history-list');
-    historyList.innerHTML = '';
+    const historyListElement = document.querySelector('.history-list');
+    historyListElement.innerHTML = '';
 
     for (const item of history.reverse()) {
-        let historyItem = document.createElement('div');
-        historyItem.classList.add('history-item');
-        historyItem.textContent = item;
-        historyItem.onclick = () => insertFromHistory(item);
-        historyList.append(historyItem);
+        let historyItemElement = document.createElement('div');
+        historyItemElement.classList.add('history-item');
+        historyItemElement.textContent = item;
+        historyItemElement.onclick = () => insertFromHistory(item);
+        historyListElement.append(historyItemElement);
     }
 }
 
 function updateHistory() {
-    const historyPanel = document.querySelector('.history');
+    const historyElement = document.querySelector('.history');
 
-    if (!historyPanel.classList.contains('shown')) return;
+    if (!historyElement.classList.contains('shown')) return;
 
     loadHistory();
 }
 
 function switchHistoryPanel() {
-    const mainPanel = document.querySelector('.main');
-    const historyPanel = document.querySelector('.history');
+    const mainElement = document.querySelector('.main');
+    const historyElement = document.querySelector('.history');
 
-    if (historyPanel.classList.contains('shown')) {
-        mainPanel.classList.remove('shifted');
-        historyPanel.classList.remove('shown');
+    if (historyElement.classList.contains('shown')) {
+        mainElement.classList.remove('shifted');
+        historyElement.classList.remove('shown');
     }
     else {
         loadHistory();
-        mainPanel.classList.add('shifted');
-        historyPanel.classList.add('shown');
+        mainElement.classList.add('shifted');
+        historyElement.classList.add('shown');
     }
 }
 
@@ -165,8 +165,8 @@ function closeHistoryMobile() {
 }
 
 function insertFromHistory(data) {
-    const inputField = document.querySelector('.converter-input');
-    inputField.value = data;
+    const inputFieldElement = document.querySelector('.converter-input');
+    inputFieldElement.value = data;
 
     closeHistoryMobile();
     showSymbolsAmount();
@@ -174,33 +174,33 @@ function insertFromHistory(data) {
 }
 
 function showSymbolsAmount() {
-    const amountField = document.querySelector('.symbols-amount');
+    const symbolsAmountElement = document.querySelector('.symbols-amount');
     const inputValueLength = document.querySelector('.converter-input').value.length;
 
-    amountField.textContent = `${inputValueLength} / ${MAX_INPUT_LENGTH}`
+    symbolsAmountElement.textContent = `${inputValueLength} / ${MAX_INPUT_LENGTH}`
 }
 
 function init() {
-    const inputField = document.querySelector('.converter-input');
-    inputField.oninput = () => {
-        if (inputField.value === '') clear();
+    const inputFieldElement = document.querySelector('.converter-input');
+    inputFieldElement.oninput = () => {
+        if (inputFieldElement.value === '') clear();
 
         showSymbolsAmount();
     };
 
-    const convertButton = document.querySelector('.convert-button');
-    convertButton.onclick = convert;
+    const convertButtonElement = document.querySelector('.convert-button');
+    convertButtonElement.onclick = convert;
 
-    const clearButton = document.querySelector('.clear-button');
-    clearButton.onclick = clear;
+    const clearButtonElement = document.querySelector('.clear-button');
+    clearButtonElement.onclick = clear;
 
-    const copyButton = document.querySelector('.copy-button');
-    copyButton.onclick = copy;
+    const copyButtonElement = document.querySelector('.copy-button');
+    copyButtonElement.onclick = copy;
 
-    const historyButton = document.querySelector('.history-button');
-    const closeHistoryButton = document.querySelector('.close-history-button');
-    historyButton.onclick = closeHistoryButton.onclick = switchHistoryPanel;
-
+    const historyButtonElement = document.querySelector('.history-button');
+    const closeHistoryButtonElement = document.querySelector('.close-history-button');
+    historyButtonElement.onclick = closeHistoryButtonElement.onclick = switchHistoryPanel;
+    
     if (!localStorage.getItem('history')) {
         localStorage.setItem('history', JSON.stringify([]));
     }
